f1b0efb8ba476b7e655dbb7749d5b1b6099b944f
Ultimate_Nickname <dallos.kristof@gmail.com>
2024-02-11 21:56:31 +0100

    Added Table model

---
+++ FoglalasAPI/FoglalasAPI/Models/Table.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoglalasAPI.Models
+{
+    [Table("Tables")]
+    public class Table
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Key]
+        [Required]
+        [ForeignKey("Restaurant_Id")]
+        public Restaurant RestaurantId { get; set; }
+        [Required]
+        public int Size { get; set; }
+    }
+}
