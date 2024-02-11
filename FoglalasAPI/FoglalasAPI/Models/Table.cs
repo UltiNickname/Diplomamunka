931496300edf75a687b34459a2faca5207f1db99
Ultimate_Nickname <dallos.kristof@gmail.com>
2024-02-11 22:03:22 +0100

    Added PrimaryKeys to Table and User models

--- FoglalasAPI/FoglalasAPI/Models/Table.cs
+++ FoglalasAPI/FoglalasAPI/Models/Table.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FoglalasAPI.Models
 {
     [Table("Tables")]
+    [PrimaryKey("Id")]
     public class Table
     {
         [Key]
--- FoglalasAPI/FoglalasAPI/Models/User.cs
+++ FoglalasAPI/FoglalasAPI/Models/User.cs
@@ -1,16 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FoglalasAPI.Models
 {
     [Table("Users")]
+    [PrimaryKey("Id")]
     public class User
     {
-        [Key]
-        public string Email { get; set; }
-        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Key]
+        public string Email { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
     }
