d4010bfbead921341538addd9f58ca14b4a832f1
Ultimate_Nickname <dallos.kristof@gmail.com>
2024-02-11 21:48:52 +0100

    Added Restaurant model

---
+++ FoglalasAPI/FoglalasAPI/Models/Restaurant.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoglalasAPI.Models
+{
+    [Table("Restaurants")]
+    public class Restaurant
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [ForeignKey("City_Id")]
+        public City City { get; set; }
+        [Required]
+        public int Capacity { get; set; }
+        [Required]
+        public bool Outdoor { get; set; }
+        [Required]
+        public bool SeperateRoom { get; set; }
+        [Required]
+        public bool FixedTables { get; set; }
+    }
+}
