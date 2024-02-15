6bc9a1fd0ed73101cb33168cb916299e14837ab5
Ultimate_Nickname <dallos.kristof@gmail.com>
2024-02-15 22:44:45 +0100

    CityController added

---
+++ FoglalasAPI/FoglalasAPI/Controllers/CityController.cs
@@ -0,0 +1,34 @@
+﻿using FoglalasAPI.Context;
+using FoglalasAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoglalasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        private AppDbContext _appDbContext;
+
+        private readonly ILogger<CityController> _logger;
+
+        public CityController(ILogger<CityController> logger, AppDbContext context)
+        {
+            _appDbContext = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IEnumerable<City> GetAllUsers()
+        {
+            return (from c in _appDbContext.Cities
+                    select new City
+                    {
+                        CityId = c.CityId,
+                        Name = c.Name
+                    }).ToList();
+        }
+    }
+}
