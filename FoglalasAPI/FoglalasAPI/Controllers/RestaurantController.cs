557e7ba3a1fa514bdf47179c680860ad0781c973
Ultimate_Nickname <dallos.kristof@gmail.com>
2024-02-15 22:46:51 +0100

    RestaurantController added

---
+++ FoglalasAPI/FoglalasAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,35 @@
+﻿using FoglalasAPI.Context;
+using FoglalasAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoglalasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+        private AppDbContext _appDbContext;
+
+        private readonly ILogger<RestaurantController> _logger;
+
+        public RestaurantController(ILogger<RestaurantController> logger, AppDbContext context)
+        {
+            _appDbContext = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IEnumerable<Restaurant> GetAllUsers()
+        {
+            return (from r in _appDbContext.Restaurants
+                    select new Restaurant
+                    {
+                        RestaurantId = r.RestaurantId,
+                        Name = r.Name,
+                        City = r.City,
+                    }).ToList();
+        }
+    }
+}
