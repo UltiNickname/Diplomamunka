0ee7d6ed77cb880308214a4a94877800fd28bd05
Ultimate_Nickname <dallos.kristof@gmail.com>
2024-02-17 00:30:01 +0100

    Edit Table model

--- FoglalasAPI/FoglalasAPI/Models/Table.cs
+++ FoglalasAPI/FoglalasAPI/Models/Table.cs
@@ -5,12 +5,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace FoglalasAPI.Models
 {
     [Table("Tables")]
+    [PrimaryKey("TableId", "RestaurantFK")]
     public class Table
     {
         [Key, Column(Order = 1)]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int TableId { get; set; }
-        [Key, Column(Order = 0)]
+        [Key, Column(Order = 1)]
         [Required]
         [ForeignKey("RestaurantFK")]
         public Restaurant Restaurant { get; set; }
