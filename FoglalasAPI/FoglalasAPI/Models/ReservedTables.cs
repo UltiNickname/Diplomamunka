72a3701c064a62fa4c35cb30d39e7cde41baee3b
Ultimate_Nickname <dallos.kristof@gmail.com>
2024-02-17 15:51:50 +0100

    ReservedTables added

---
+++ FoglalasAPI/FoglalasAPI/Models/ReservedTables.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoglalasAPI.Models
+{
+    public class ReservedTables
+    {
+        [Required]
+        [ForeignKey("ReservationFK")]
+        public Reservation Reservation { get; set; }
+        [Required]
+        [ForeignKey("TableFK")]
+        public Table Table { get; set; }
+        [Required]
+        public int Count { get; set; }
+    }
+}
