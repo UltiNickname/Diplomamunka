7a85fe7ea4f4c4c5d30b544cd9f16cf825d52d8f
Ultimate_Nickname <dallos.kristof@gmail.com>
2024-02-17 16:00:42 +0100

    Removed Primary key

--- FoglalasAPI/FoglalasAPI/Models/ReservedTables.cs
+++ FoglalasAPI/FoglalasAPI/Models/ReservedTables.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FoglalasAPI.Models
 {
-    [PrimaryKey("Reservation", "Table")]
     public class ReservedTables
     {
         [Required]
--- FoglalasAPI/FoglalasAPI/Models/RestaurantTables.cs
+++ FoglalasAPI/FoglalasAPI/Models/RestaurantTables.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FoglalasAPI.Models
 {
-    [PrimaryKey("Restauarnt", "Table")]
     public class RestaurantTables
     {
         [Required]
