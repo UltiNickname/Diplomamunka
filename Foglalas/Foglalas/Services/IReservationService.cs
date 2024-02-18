214c315eddd3792fe3138568e64a3abc95ce96d1
Ultimate_Nickname <dallos.kristof@gmail.com>
2024-02-19 00:56:33 +0100

    IReservationService created

---
+++ Foglalas/Foglalas/Services/IReservationService.cs
@@ -0,0 +1,14 @@
+﻿using Foglalas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foglalas.Services
+{
+    public interface IReservationService
+    {
+        Task<string> Reserve(Restaurant restaurant, User user, int size, DateTime date, bool outdoor, bool seperate);
+    }
+}
