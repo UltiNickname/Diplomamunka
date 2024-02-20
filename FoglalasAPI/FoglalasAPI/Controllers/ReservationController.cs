cbecd3b7c8a867c4e4bc4e318a8bcc4c54098f73
Ultimate_Nickname <dallos.kristof@gmail.com>
2024-02-20 12:04:05 +0100

    ReservationRepetition deletet (temporary)

--- FoglalasAPI/FoglalasAPI/Controllers/ReservationController.cs
+++ FoglalasAPI/FoglalasAPI/Controllers/ReservationController.cs
@@ -28,17 +28,10 @@ namespace FoglalasAPI.Controllers
             _logger = logger;
         }
 
-        public bool ReservationRepeated(Reservation reservation)
-        {
-            return _appDbContext.Reservations.Where(r => r.Equals(reservation)).Any();
-        }
-
         [HttpPost]
         [Route("AddNewReservation")]
         public async Task<IActionResult> SaveReservation(Reservation reservation)
         {
-            if (ReservationRepeated(reservation))
-                return BadRequest("This reservation is already exist");
             _appDbContext.Reservations.Add(reservation);
             _appDbContext.SaveChanges();
             return Ok("Reservation saved!");
