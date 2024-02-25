c9714e35d0937a467c0b6f88d872978bcbeb10e7
Ultimate_Nickname <dallos.kristof@gmail.com>
2024-02-25 13:26:38 +0100

    Table optimalization Complete

--- FoglalasAPI/FoglalasAPI/Controllers/ReservationController.cs
+++ FoglalasAPI/FoglalasAPI/Controllers/ReservationController.cs
@@ -83,27 +83,31 @@ namespace FoglalasAPI.Controllers
                 SeperateRoom = reservation.SeperateRoom,
             };
             _appDbContext.Reservations.Add(dbReservation);
-
-            
-            List<Table> tables = _appDbContext.Tables.ToList();
+            _appDbContext.SaveChanges();
             int size = reservation.Size;
-            int num = 0;
+            List<Table> tables = _appDbContext.Tables.ToList();
+            tables.Reverse();
+            int num;
             foreach (Table table in tables)
             {
-                int availableTables = (from rtt in _appDbContext.RestaurantTables
+                num = 0;
+                var availableTables = (from rtt in _appDbContext.RestaurantTables
                                        join t in _appDbContext.Tables on rtt.TableId equals t.TableId
                                        join rv in _appDbContext.Reservations on rtt.RestaurantId equals rv.Restaurant.RestaurantId
                                        join rvt in _appDbContext.ReservedTables on rv.ReservationId equals rvt.ReservationId
-                                       where t.Size == table.Size && rv.Restaurant.RestaurantId == reservation.ReservationId
-                                       group new { Capacity = rtt.Count, reservedTableCount = rvt.Count } by new { rtt.Count, reservedTableCount = rvt.Count } into g
-                                       select g.Key.Count - g.Sum(x => x.reservedTableCount)).ToList().First();
-                
-                while(size > 0 && size > table.Size && (availableTables > 0 || availableTables == null))
+                                       where t.TableId == table.TableId && rtt.TableId == table.TableId && rtt.RestaurantId == rv.Restaurant.RestaurantId && rvt.TableId == table.TableId
+                                       group new { RestaurantTableCount = rtt.Count, ReservedTableCount = rvt.Count } 
+                                       by new { restaurantTableCount = rtt.Count, reservedTableCount = rvt.Count } into g
+                                       orderby g.Key.restaurantTableCount descending
+                                       select g.Key.restaurantTableCount - g.Sum(x => x.ReservedTableCount)).ToList();
+
+                while (size > 0 && size >= table.Size && (availableTables.Count() == 0 || availableTables.First() > 0))
                 {
                     num++;
                     size-=table.Size;
                 }
-                _appDbContext.Database.ExecuteSqlRaw($"INSERT INTO \"RestaurantTables\"(\"ReservationId\",\"TableId\",\"Count\") VALUES ({reservation.ReservationId},{table.TableId},{num})");   
+                if(num > 0)
+                _appDbContext.Database.ExecuteSqlRaw($"INSERT INTO \"ReservedTables\"(\"ReservationId\",\"TableId\",\"Count\") VALUES ({dbReservation.ReservationId},{table.TableId},{num})");   
             }
             _appDbContext.SaveChanges();
             return Ok("Reservation saved!");
